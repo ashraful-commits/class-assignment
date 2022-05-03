ed261d6d05e68205cc7df273972cef0ce04c3c47
ashraful <ashrafulalam10000@gmail.com>
2022-05-03 12:42:50 +0600

    class 8 assignment

---
+++ app.js
@@ -0,0 +1,141 @@
+ // Create a complete fifth class students array and object data structure and find their result with gpa, grade, cgpa and final result  
+
+const Class_five =[
+    {
+        Name:'Md.Shihab',
+        Roll:1,
+        Gender:'Male',
+        Ban:55,
+        Eng:65,
+        Math:33,
+        Sc:70,
+        S_Sc:60,
+        Rel:80
+    },
+    {
+        Name:'Abdur Razzak',
+        Roll:2,
+        Gender:'Male',
+        Ban:50,
+        Eng:60,
+        Math:43,
+        Sc:80,
+        S_Sc:60,
+        Rel:40
+    },
+    {
+        Name:'Md.Raza',
+        Roll:3,
+        Gender:'Male',
+        Ban:53,
+        Eng:62,
+        Math:53,
+        Sc:75,
+        S_Sc:40,
+        Rel:90
+    },
+    {
+        Name:'Emran Hasan',
+        Roll:4,
+        Gender:'Male',
+        Ban:70,
+        Eng:68,
+        Math:73,
+        Sc:40,
+        S_Sc:50,
+        Rel:60
+    },
+    {
+        Name:'Md.Anisul HOque',
+        Roll:5,
+        Gender:'Male',
+        Ban:59,
+        Eng:69,
+        Math:43,
+        Sc:70,
+        S_Sc:90,
+        Rel:40
+    },
+    {
+        Name:'Md.Rubel',
+        Roll:6,
+        Gender:'Male',
+        Ban:40,
+        Eng:50,
+        Math:93,
+        Sc:80,
+        S_Sc:30,
+        Rel:60
+    },
+    {
+        Name:'Somaiya alam asha',
+        Roll:7,
+        Gender:'female',
+        Ban:52,
+        Eng:66,
+        Math:39,
+        Sc:30,
+        S_Sc:90,
+        Rel:90
+    },
+    {
+        Name:'Istiak Robin',
+        Roll:8,
+        Gender:'Male',
+        Ban:55,
+        Eng:65,
+        Math:33,
+        Sc:70,
+        S_Sc:60,
+        Rel:80
+    },
+    {
+        Name:'Md.Mahbubur Rahman',
+        Roll:9,
+        Gender:'Male',
+        Ban:50,
+        Eng:60,
+        Math:53,
+        Sc:90,
+        S_Sc:90,
+        Rel:40
+    },
+    {
+        Name:'Zahid Hasan',
+        Roll:10,
+        Gender:'Male',
+        Ban:55,
+        Eng:65,
+        Math:33,
+        Sc:70,
+        S_Sc:60,
+        Rel:80
+    },
+]
+
+
+Class_five.forEach((data,index)=>{
+console.log(`${data}`)
+    } 
+ )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
